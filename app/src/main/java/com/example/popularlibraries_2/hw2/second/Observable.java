58b49158c371d8ed6a1823db0edf32ebe60c6354
bahu4 <bahu4@mail.ru>
2020-07-03 00:45:37 +0300

    hw3 second tusk

--- app/src/main/java/com/example/popularlibraries_2/hw2/second/Observable.java
+++
@@ -1,9 +0,0 @@
-package com.example.popularlibraries_2.hw2.second;
-
-public interface Observable {
-    void registerObserver(Observer observer);
-
-    void unregisterObserver(Observer observer);
-
-    void notifyAllObservers();
-}
--- app/src/main/java/com/example/popularlibraries_2/hw2/second/Observer.java
+++
@@ -1,5 +0,0 @@
-package com.example.popularlibraries_2.hw2.second;
-
-public interface Observer {
-    void updateData(String msg);
-}
