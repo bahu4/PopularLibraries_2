13ceb9c07aab460184c5e565956dcd1c8143a444
bahu4 <bahu4@mail.ru>
2020-07-30 22:40:54 +0300

    hw6 task1

---
+++ app/src/main/java/com/example/popularlibraries_2/hw6/task1/Green.java
@@ -0,0 +1,9 @@
+package com.example.popularlibraries_2.hw6.task1;
+
+public class Green {
+    String color = "green";
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ app/src/main/java/com/example/popularlibraries_2/hw6/task2/DiActivity.java
@@ -0,0 +1,16 @@
+package com.example.popularlibraries_2.hw6.task2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.popularlibraries_2.R;
+
+public class DiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_di);
+    }
+}
